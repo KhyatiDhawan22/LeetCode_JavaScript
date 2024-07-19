785b89b472fcd4a792da0e4124fade448ecf7957
Khyati Dhawan <112753105+KhyatiDhawan22@users.noreply.github.com>
2024-07-19 22:20:56 +0530

    Time: 156 ms (43.87%) | Memory: 79.2 MB (33.56%) - LeetSync

---
+++ 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
@@ -0,0 +1,12 @@
+function flat(arr, n) {
+  const result = [];
+  for (let item of arr) {
+    if (Array.isArray(item) && n > 0) {
+      result.push(...flat(item, n - 1)); 
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
